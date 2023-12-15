d4613408e83693698dfc4ec525886c469ba180a2
wwlouis00 <wwlouis00@gmail.com>
2023-12-16 03:55:14 +0800

    Update segment_basic.py

--- segment_basic.py
+++ segment_basic.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 # from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 import cv2
-model = YOLO("model/yolov8m-seg.pt")
+model = YOLO("model/yolov8n-seg.pt")
 
 result = model.predict(source='0',show=True)
-# result = model.predict(source="/home/wwlouis/project/yolov8/video/frame_60488.jpg",project="result/detection/",name="package",show=True,save=True)
+# result = model.predict(source="your video or image",project="save dir",name="file name",show=True,save=True)
 print(result)
