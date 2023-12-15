7e9913601914a054e32be267dc3b4739320c2897
wwlouis00 <wwlouis00@gmail.com>
2023-12-16 03:56:13 +0800

    Update segment_package_realtime.py

--- segment_package_realtime.py
+++ segment_package_realtime.py
@@ -4,5 +4,4 @@ import cv2
 model = YOLO("model/package_1030_n.pt")
 
 result = model.predict(source='0',show=True)
-# result = model.predict(source="/home/wwlouis/project/yolov8/video/frame_60488.jpg",project="result/detection/",name="package",show=True,save=True)
 print(result)
