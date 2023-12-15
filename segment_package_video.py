d08d3cf4cab4408abdfe4e6a1e0473794ce84127
wwlouis00 <wwlouis00@gmail.com>
2023-12-16 03:53:57 +0800

    Update segment_package_video.py

--- segment_package_video.py
+++ segment_package_video.py
@@ -4,6 +4,6 @@ import cv2
 
 model = YOLO("package_1215_n.pt")
 
-# result = model.predict(source="/home/wwlouis/project/Dataset/兆輝科技物流影片/智慧物流/05_12_19_00_44.mp4",project="result/segment/",name="package",show=False,save=True)
-result = model.predict(source="/home/wwlouis/project/Dataset/兆輝科技物流影片/智慧物流/05_12_19_00_44.mp4",show=True)
+# result = model.predict(source=""your video"",name="package",show=False,save=True)
+result = model.predict(source="your video",show=True)
 print(result)
